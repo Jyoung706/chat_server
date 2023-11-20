c1444ff2702280a30bf3405b25a4657b27d9832a
Jyoung706 <wnsdud9476@gmail.com>
2023-11-20 22:34:44 +0900

    chat

--- utils/io.js
+++ utils/io.js
@@ -1,3 +1,4 @@
+const chatController = require("../Controllers/chat.controller");
 const userController = require("../Controllers/user.controller");
 
 module.exports = function (io) {
@@ -6,12 +7,28 @@ module.exports = function (io) {
     console.log("client is connected", socket.id);
 
     socket.on("login", async (userName, cb) => {
-      console.log("userName : ", userName);
       try {
         const user = await userController.saveUser(userName, socket.id);
+        const welcomeMessage = {
+          chat: `${user.name} is joined to this room`,
+          user: { id: null, name: "system" },
+        };
+        io.emit("message", welcomeMessage);
         cb({ ok: true, data: user });
       } catch (e) {
         console.error(e);
+        cb({ ok: false, error: e.message });
+      }
+    });
+
+    socket.on("sendMessage", async (message, cb) => {
+      try {
+        const user = await userController.checkUser(socket.id);
+        const newMessage = await chatController.saveChat(message, user);
+        io.emit("message", newMessage);
+        cb({ ok: true });
+      } catch (e) {
+        console.log(e);
         cb({ ok: false, error: e.message });
       }
     });
